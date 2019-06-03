de1e714d45f4f88910622624c94b00148c47a2bd
mid <673601015@qq.com>
2019-06-03 10:35:00 +0800

    Upload test

---
+++ xiyou_full_platform/src/main/java/com/fznsys/xiyou_full_platform/XiyouFullPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.fznsys.xiyou_full_platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XiyouFullPlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XiyouFullPlatformApplication.class, args);
+    }
+
+}
