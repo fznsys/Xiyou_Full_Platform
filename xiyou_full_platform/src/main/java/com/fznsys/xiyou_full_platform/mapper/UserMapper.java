1508412750b8393a46624ad4cde1c449528fe557
mid <673601015@qq.com>
2019-06-06 08:39:25 +0800

    1

--- xiyou_full_platform/src/main/java/com/fznsys/xiyou_full_platform/mapper/UserMapper.java
+++ xiyou_full_platform/src/main/java/com/fznsys/xiyou_full_platform/mapper/UserMapper.java
@@ -11,25 +11,25 @@ import java.util.ArrayList;
 @Repository("userMapper")
 public interface UserMapper {
 
-    @Select("select * from tb_user where username=#{username} and password=md5(#{password})")
+    @Select("select * from tb_user_user where username=#{username} and password=md5(#{password})")
     User LoginByUsernameAndPassword(@Param("username") String username, @Param("password") String password);
 
-    @Select("  SELECT * FROM tb_user WHERE username = #{username}")
+    @Select("  SELECT * FROM tb_user_user WHERE username = #{username}")
     User findByUsername(String username);
 
-    @Select(" SELECT * FROM tb_user")
+    @Select(" SELECT * FROM tb_user_user")
     ArrayList<User> findAll();
 
-    @Select("select * from tb_user where id=#{id}")
+    @Select("select * from tb_user_user where id=#{id}")
     User findById(String id);
 
-    @Update("update tb_user set username = #{username}, password = #{password}, role=#{role}  where id = #{id}")
+    @Update("update tb_user_user set username = #{username}, password = #{password}, role=#{role}  where id = #{id}")
     void update(User user);
 
-    @Delete("delete from tb_user where  id=#{id,jdbcType=INTEGER}")
+    @Delete("delete from tb_user_user where  id=#{id,jdbcType=INTEGER}")
     String delete(User user);
 
-    @Insert("insert into tb_user(username, password, role) values (#{username},#{password},#{role})")
+    @Insert("insert into tb_user_user(username, password, role) values (#{username},#{password},#{role})")
     String insert(User user);
 
 }
